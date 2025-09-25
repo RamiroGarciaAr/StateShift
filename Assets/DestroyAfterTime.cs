5311fa17d105b6045ea5616e44a4b506029a187a
Ramiro Garcia <67532049+RamiroGarciaAr@users.noreply.github.com>
2025-09-24 21:48:11 -0300

    Added Addon

---
+++ Assets/DestroyAfterTime.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    [SerializeField] private float lifetime = 5f; 
+
+    private void OnEnable()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
+
