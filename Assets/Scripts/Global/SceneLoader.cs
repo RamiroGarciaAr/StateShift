fc3adb27a4f879919927bb4f930aac228a1b3f78
Nicky <113854943+nicky-5@users.noreply.github.com>
2025-09-30 20:21:20 -0300

    Add SceneLoader reload function and documentation

--- Assets/Scripts/Global/SceneLoader.cs
+++ Assets/Scripts/Global/SceneLoader.cs
@@ -1,23 +1,40 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Global loading scene management.
+/// </summary>
 static class SceneLoader
 {
     private static string _nextScene;
 
-    public static void OpenLoadingScene(string nextSceneName)
+    /// <summary>
+    /// Asynchronously loads LoadingScene with the next scene to load.
+    /// </summary>
+    /// <param name="sceneName">Next scene to load.</param>
+    /// <returns>LoadingScene async load operation.</returns>
+    public static AsyncOperation SwitchSceneAsync(string sceneName)
     {
-        _nextScene = nextSceneName;
-        SceneManager.LoadScene("LoadingScene");
+        _nextScene = sceneName;
+        return SceneManager.LoadSceneAsync("LoadingScene");
     }
 
+    /// <summary>
+    /// Asynchronously loads LoadingScene with the current scene to load.
+    /// </summary>
+    /// <returns>LoadingScene async load operation.</returns>
+    public static AsyncOperation ReloadSceneAsync()
+    {
+        return SwitchSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    /// Asynchronously loads the next scene.
+    /// </summary>
+    /// <remarks>DO NOT CALL OUTSIDE OF LoadingScene.</remarks>
+    /// <returns>Next scene async load operation.</returns>
     public static AsyncOperation LoadNextAsync()
     {
         return SceneManager.LoadSceneAsync(_nextScene);
-    }
-
-    public static AsyncOperation CloseLoadingScene()
-    {
-        return SceneManager.UnloadSceneAsync("LoadingScene");
     }
 }
