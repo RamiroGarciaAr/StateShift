b2418ab49000208e13cf06ae2fc0d50942137856
Ramiro Garcia <67532049+RamiroGarciaAr@users.noreply.github.com>
2026-07-20 03:04:16 -0300

    Update AIBrain.cs

--- Assets/Scripts/AI/AIBrain.cs
+++ Assets/Scripts/AI/AIBrain.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using Health;
 using UnityEngine;
 
-public class AIBrain : MonoBehaviour
+[RequireComponent(typeof(AIMemory))]
+[RequireComponent(typeof(EnemyHealth))]
+public class AIBrain : MonoBehaviour, ITickable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private AIPerception perception;
+
+    private AIMemory _memory;
+    private EnemyHealth _health;
+
+    public bool IsTickable => true;
+
+    public void OnTick(float dt) => _memory.Report(perception.Sample());
+
+    private void Awake()
     {
-        
+        _memory = GetComponent<AIMemory>();
+        _health = GetComponent<EnemyHealth>();
+
+        if (perception == null)
+        {
+            Debug.LogError($"[AIBrain] No Perception assigned on {gameObject.name}", this);
+            enabled = false;
+            return;
+        }
+        _health.OnDeath += HandleDeath;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (_health != null)
+            _health.OnDeath -= HandleDeath;
+    }
+
+    private void OnEnable()
+    {
+        if (AITickManager.Instance == null)
+        {
+            Debug.LogError("[AI Tick] No tick manager was found", this);
+            return;
+        }
+        AITickManager.Instance.RegisterAgent(this);
+    }
+
+    private void OnDisable()
+    {
+        AITickManager.Instance?.UnregisterAgent(this);
+    }
+
+    private void HandleDeath()
+    {
+        enabled = false;
     }
 }
